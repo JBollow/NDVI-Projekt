71002bab89210bf74d79be00f9463d76585589c1
Jan-Patrick Bollow <Jan_PatrickBollow@hotmail.com>
2021-04-30 08:38:22 +0200

    test

--- server.py
+++ server.py
@@ -42,13 +42,16 @@ def ndvi():
     cir_file_path = os.path.abspath(os.path.join(localPath, cirPath, filename))
 
     time.sleep(3)
+
     img = cv2.imread(cir_file_path)
+    print("image read")
 
     if img is None:
         print('Could not open or find the image:')
         return json.dumps(failed)
 
-    img = cv2.normalize(img, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+    # img = cv2.normalize(img, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+    # img = cv2.normalize(img, img, 55, 310, cv2.NORM_MINMAX)
     img = cv2.filter2D(img, -1, kernel)
     img = cv2.resize(img, (1600, 1200))
 
