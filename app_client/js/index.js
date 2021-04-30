8895c480edcb0ab045542b4f24e93ee99cc83a9e
Jan-Patrick Bollow <Jan_PatrickBollow@hotmail.com>
2021-04-30 08:26:35 +0200

    Test

--- app_client/js/index.js
+++ app_client/js/index.js
@@ -17,7 +17,8 @@ router.get("/capture", function (req, res) {
         var pic_name = result1.Function.File[0].NAME[0];
         var pic_path = "http://192.168.1.254/DCIM/PHOTO/" + pic_name;
         var postjson = { filename: pic_name };
-
+        
+        console.log("Start Jimp");
         Jimp.read(pic_path)
           .then((img) => {
             return img.write("./app_client/CIR_Temp/cir.jpg");
